c7c062c5607e88d47a86415531e79b15cf08c7ce
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2024-04-26 20:59:40 +0530

    cors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const connectDB = require("./db/conn");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors()
+);
 
 // mongoose.connect('mongodb+srv://codewithbakhtiyar:Ahmad@123@cluster0.jjnoit8.mongodb.net/')
 // .then(() => console.log('Connected to MongoDB'))
 // .catch(err => console.error('Failed to connect to MongoDB:', err));
 
-
 // Include the router for API endpoints
-const dataRouter = require('./routes/router');
-app.use('/api/data', dataRouter);
+const dataRouter = require("./routes/router");
+app.use("/api/data", dataRouter);
 
 connectDB()
   .then(() => {
@@ -27,7 +27,3 @@ connectDB()
   .catch((err) => {
     console.error("Error connecting to database:", err);
   });
-
-
-
-
