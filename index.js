c4bb0179b15a1f9e6fb5c653ad2e594098054982
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2024-08-19 21:21:27 +0530

    Revert "updatedAPI"
    This reverts commit 2f5db1b14e005120245043a3b3bdad112f495868.

--- index.js
+++ index.js
@@ -1,102 +1,22 @@
 const express = require("express");
-const connectDB = require("./db/conn"); // Import the connectDB function
-const User = require("./models/User");
-const Product = require('./models/Product')
-const faqController = require('./controllers/faqController');
-const AuthController = require('./controllers/AuthController');
+const cors = require("cors");
+const connectDB = require("./db/conn");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors()
+);
 
-//Routes
-app.post('/faqs', faqController.createFAQ);
-app.get('/faqs/category/:category', faqController.getFAQsByCategory);
-app.post('/login', AuthController.login);
-app.post('/register', AuthController.register);
+// mongoose.connect('mongodb+srv://codewithbakhtiyar:Ahmad@123@cluster0.jjnoit8.mongodb.net/')
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error('Failed to connect to MongoDB:', err));
 
-app.get("/user", async (req, res) => {
-  try {
-    const usersData = await User.find();
-    res.send(usersData);
-  } catch (e) {
-    res.send(e);
-  }
-});
-
-app.get("/user/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const userData = await User.findById(_id);
-    console.log(userData)
-    if (!userData) {
-      return res.status(404).send();
-    } else {
-      res.send(userData);
-    }
-  } catch (e) {
-    res.send(e);
-  }
-});
-
-// Create a user
-// app.post("/register", async (req, res) => {
-//   try {
-//     const user = new User(req.body);
-//     let result = await user.save();
-//     result = result.toObject;
-//     delete result.password;
-//     res.status(201).send(user);
-
-//   } catch (error) {
-//     console.error("Error creating user:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// app.post("/login", async (req, res) => {
-//   try {
-  
-//     if (req.body.email && req.body.password) {
-//       let user = await User.findOne(req.body).select("-password");
-//       if (user) {
-//         res.send(JSON.stringify(user));
-//       } else {
-//         res.status(404).send({ error: "No user found" });
-//       }
-//     } else {
-//       res.status(400).send({ error: "Invalid request" });
-//     }
-//   } catch (error) {
-//     console.error("Error logging in:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-
-
-
-
-
-app.delete("/user/:id", async(req,res) =>{
-  try{
-    const deleteUser = await User.findByIdAndDelete(req.params.id);
-    if(!req.params.id){
-      return res.status(400).send()
-    }
-    res.send(deleteUser)
-    }catch(e){
-      res.status(500).send(e);
-    }
-     })
-
-  app.post("/add-product", async (req, res)=>{
-    let product = new Product(req.body);
-    let result = await product.save();
-    res.send(result)
-  })   
+// Include the router for API endpoints
+const dataRouter = require("./routes/router");
+app.use("/api/data", dataRouter);
 
 connectDB()
   .then(() => {
