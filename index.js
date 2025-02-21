2b9175fe6b53f5b850e40a54e287c4d5177dd3ce
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2025-02-21 22:10:24 +0530

    again fix

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
 app.use(express.json());
 
 // Routes
