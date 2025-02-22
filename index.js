b52129359b1926fbe9ff72670717b37197d2ef1b
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2025-02-22 13:17:31 +0530

    cors error vercel json fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const attendanceRoutes = require("./routes/attendanceRoutes")
 const authRoutes = require("./routes/authRoutes");
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,14 +16,12 @@ const corsOptions = {
   allowedHeaders: "Content-Type,Authorization",
   credentials: true, // Allow credentials like tokens/cookies
 };
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); 
 app.use(express.json());
 
 // Routes
-//app.use("/api/course", onlineRoutes);
-//app.use("/api/post", blogRoutes);
-//app.use("/api/items", itemRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/auth", authRoutes);
 
@@ -31,11 +30,9 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URI); // Use MONGO_URI from .env
   console.log("Database Connected");
 }
-
 main().catch((err) => console.log(err));
 
 // Start the server
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
