a494b2a5f5e9dbfb91ba432e8d80c8f08e17eb88
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2025-02-22 09:44:54 +0530

    cors errro fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['https://edublends-lms.vercel.app', 'http://localhost:3000'],
+  origin: ['*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
